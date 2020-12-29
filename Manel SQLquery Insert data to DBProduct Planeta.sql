785da2a15d0c6d2d012984e06b43d3b83390e77a
maneldvl <76442274+maneldvl@users.noreply.github.com>
2020-12-29 12:59:01 +0100

    Update Manel SQLquery Insert data to DBProduct Planeta.sql

--- Manel SQLquery Insert data to DBProduct Planeta.sql
+++ Manel SQLquery Insert data to DBProduct Planeta.sql
@@ -1,5 +1,5 @@
 
---Script SQL para inserción de datos en la tabla de la base de datos DB_Products
+--Script SQL para inserción de datos en las tablas de la base de datos
 
 --NOTA: Todos los comentarios en código se harán por norma en Inglés esto unifica el trabajo en equipos internacionales así como los
 -- nombres de las bases de datos, las tablas y campos 
@@ -14,32 +14,33 @@ IF EXISTS ( SELECT [name] FROM sys.databases WHERE [name] = 'DBProducts' )
 		Product_ID INT PRIMARY KEY IDENTITY (1, 1),
 		Product_Name NVARCHAR(100) NOT NULL,
 		Product_REF NVARCHAR(100) NOT NULL,
-		Product_ProductSectorIDCODE INT NULL,
+		Product_Price FLOAT NULL,
 		Product_Creation_Date DATETIME NOT NULL
 		);
 		END 
-	ELSE
-		BEGIN
-		--Create registers on Table Product
-		IF (NOT (SELECT Count(*) FROM Product) = 0 )
-			BEGIN
-				
-			DECLARE @PRODUCT_FIELDS table(
-					ROWID							INT NOT NULL identity(1,1), --Product_ID
-					Product_Name					NVARCHAR(100),
-					Product_REF						NVARCHAR(100),
-					Product_Price					FLOAT,
-					Product_Creation_Date			DATETIME
-					);  
+	
 
-			--Libros
-			--IF (NOT (SELECT Count(*) FROM Product) = 0  )
-				--BEGIN
-				INSERT INTO @PRODUCT_FIELDS VALUES ('El asesino sangriento del Planeta Perdido', 'REF-1', 20.4, GETDATE());
-				INSERT INTO @PRODUCT_FIELDS VALUES ('EL llanero solitario', 'REF-2', 10.4, GETDATE());
-				INSERT INTO @PRODUCT_FIELDS VALUES ('La abuela y el sobrino', 'REF-3', 30, GETDATE());
-				INSERT INTO @PRODUCT_FIELDS VALUES ('Navidades sangrientas', 'REF-4', 40, GETDATE());
-				--END 
-			END
+	--Create registers on Table Product
+	IF ((SELECT Count(*) FROM dbo.Product) = 0 )
+		BEGIN
+		
+		--Books
+				
+		INSERT INTO dbo.Product
+		(Product_Name, Product_REF, Product_Price, Product_Creation_Date)
+		VALUES ('El asesino sangriento del Planeta Perdido', 'REF-1', 20.4, GETDATE());
+
+		INSERT INTO dbo.Product 
+		(Product_Name, Product_REF, Product_Price, Product_Creation_Date)
+		VALUES ('EL llanero solitario', 'REF-2', 10.4, GETDATE());
+
+		INSERT INTO dbo.Product 
+		(Product_Name, Product_REF, Product_Price, Product_Creation_Date)
+		VALUES ('La abuela y el sobrino', 'REF-3', 30, GETDATE());
+
+		INSERT INTO dbo.Product 
+		(Product_Name, Product_REF, Product_Price, Product_Creation_Date)
+		VALUES ('Navidades sangrientas', 'REF-4', 40, GETDATE());
+			
 		END
-	END 
+END	
